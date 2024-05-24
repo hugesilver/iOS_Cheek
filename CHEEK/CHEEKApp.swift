c642e142f2be34b11d22794c74aca35fef64b626
hugesilver <xor3160@naver.com>
2024-05-24 20:33:41 +0900

    Initial Commit

---
+++ CHEEK/CHEEKApp.swift
@@ -0,0 +1,17 @@
+//
+//  CHEEKApp.swift
+//  CHEEK
+//
+//  Created by 김태은 on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct CHEEKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CHEEK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CHEEK
+//
+//  Created by 김태은 on 5/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
