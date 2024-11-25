7358e127a5d604eef557f0f5bf54d10559126a78
hugesilver <xor3160@naver.com>
2024-11-26 03:58:53 +0900

    Feat: Add terms of service view

--- CHEEK/Views/Welcome/WelcomeView.swift
+++ CHEEK/Views/Welcome/WelcomeView.swift
@@ -15,6 +15,8 @@ struct WelcomeView: View {
     @StateObject private var kakaoAuthViewModel: KakaoAuthViewModel = KakaoAuthViewModel()
     @StateObject private var appleAuthViewModel: AppleAuthViewModel = AppleAuthViewModel()
     
+    @State var showTermsOfService: Bool = true
+    
     @State var isMentor: Bool?
     
     @State private var isLoading: Bool = false
@@ -78,6 +80,12 @@ struct WelcomeView: View {
                 }
                 .padding(.horizontal, 16)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+                if showTermsOfService {
+                    AggrementView(
+                        onTapDone: { showTermsOfService = false }
+                    )
+                }
                 
                 if isLoading {
                     LoadingView()
@@ -150,6 +158,7 @@ struct WelcomeView: View {
     }
 }
 
+// 소셜 로그인 버튼
 struct WelcomeViewSocialButton: View {
     var text: String
     var textColor: Color
@@ -182,6 +191,132 @@ struct WelcomeViewSocialButton: View {
             RoundedRectangle(cornerRadius: 8)
                 .fill(bgColor)
         )
+    }
+}
+
+// 약관 동의
+struct AggrementView: View {
+    var onTapDone: () -> Void
+    
+    @State private var isServiceAgreed: Bool = false
+    @State private var showServiceAgreement: Bool = false
+    
+    @State private var isPrivacyAgreed: Bool = false
+    @State private var showPrivacyAgreement: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Color(red: 0, green: 0, blue: 0).opacity(0.4)
+            
+            Group {
+                VStack(spacing: 0) {
+                    Text("CHEEK! 약관동의")
+                        .headline1(font: "SUIT", color: .cheekTextNormal, bold: true)
+                        .padding(.bottom, 24)
+                    
+                    VStack(spacing: 16) {
+                        // 서비스 이용약관 동의
+                        HStack(spacing: 8) {
+                            Button(action: {
+                                isServiceAgreed.toggle()
+                            }) {
+                                if isServiceAgreed {
+                                    Circle()
+                                        .frame(width: 24, height: 24)
+                                        .foregroundColor(.cheekMainNormal)
+                                        .overlay(
+                                            Image("IconCheck")
+                                                .resizable()
+                                                .frame(width: 16, height: 16)
+                                                .foregroundColor(.cheekTextNormal)
+                                        )
+                                } else {
+                                    Circle()
+                                        .fill(.cheekTextAssitive)
+                                        .frame(width: 24, height: 24)
+                                }
+                            }
+                            
+                            Text("(필수) 서비스 이용약관 동의")
+                                .body1(font: "SUIT", color: .cheekTextNormal, bold: false)
+                            
+                            Spacer()
+                            
+                            Button(action: {
+                                showServiceAgreement.toggle()
+                            }) {
+                                Text("보기")
+                                    .body1(font: "SUIT", color: .cheekTextAlternative, bold: true)
+                            }
+                        }
+                        
+                        // 개인정보 수집 및 이용 동의
+                        HStack(spacing: 8) {
+                            Button(action: {
+                                isPrivacyAgreed.toggle()
+                            }) {
+                                if isPrivacyAgreed {
+                                    Circle()
+                                        .frame(width: 24, height: 24)
+                                        .foregroundColor(.cheekMainNormal)
+                                        .overlay(
+                                            Image("IconCheck")
+                                                .resizable()
+                                                .frame(width: 16, height: 16)
+                                                .foregroundColor(.cheekTextNormal)
+                                        )
+                                } else {
+                                    Circle()
+                                        .fill(.cheekTextAssitive)
+                                        .frame(width: 24, height: 24)
+                                }
+                            }
+                            
+                            Text("(필수) 개인정보 수집 및 이용 동의")
+                                .body1(font: "SUIT", color: .cheekTextNormal, bold: false)
+                            
+                            Spacer()
+                            
+                            Button(action: {
+                                showPrivacyAgreement.toggle()
+                            }) {
+                                Text("보기")
+                                    .body1(font: "SUIT", color: .cheekTextAlternative, bold: true)
+                            }
+                        }
+                    }
+                    .padding(.bottom, 32)
+                    
+                    if isServiceAgreed && isPrivacyAgreed {
+                        Button(action: {
+                            onTapDone()
+                        }) {
+                            ButtonActive(text: "확인")
+                        }
+                    } else {
+                        ButtonDisabled(text: "확인")
+                    }
+                }
+                .padding(.horizontal, 16)
+                .padding(.vertical, 32)
+                .background(
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(.cheekWhite)
+                )
+            }
+            .padding(16)
+        }
+        .ignoresSafeArea()
+        .sheet(isPresented: $showServiceAgreement) {
+            WebView(url: "https://malleable-can-825.notion.site/Cheek-ed7d2691b18948a2bf4fbeb0c22dff36?pvs=4")
+                .presentationDragIndicator(.hidden)
+                .presentationDetents([.fraction(0.9)])
+        }
+        .sheet(isPresented: $showPrivacyAgreement) {
+            WebView(url: "https://malleable-can-825.notion.site/cf2b03b6bed24dbebc866fa3cf0a1c6b?pvs=4")
+                .presentationDragIndicator(.hidden)
+                .presentationDetents([.fraction(0.9)])
+        }
     }
 }
 
